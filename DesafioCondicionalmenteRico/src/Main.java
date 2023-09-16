4af965756a7d9de50a2ff0478f454ddf5a5c2e52
Carla Franco <carlapriscila15@hotmail.com>
2023-09-16 10:42:11 -0300

    adicionando desafio condicionalmente rico

---
+++ DesafioCondicionalmenteRico/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int saldoTotal = scanner.nextInt();
+        int valorSaque = scanner.nextInt();
+
+        if (valorSaque <= saldoTotal) {
+
+            int novoSaldo = saldoTotal - valorSaque;
+
+            System.out.println("Saque realizado com sucesso. Novo saldo: " + novoSaldo);
+
+        } else {
+            System.out.println("Saldo insuficiente. Saque nao realizado! ");
+
+        }
+    }
+}
