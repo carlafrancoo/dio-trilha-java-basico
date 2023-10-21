78959935df4e918367cfa6e58f7df8014aa628ee
Carla Franco <carlapriscila15@hotmail.com>
2023-10-21 16:12:23 -0300

    adicionando desafio abrindo contas

---
+++ DesafioAbrindoContas/src/Desafio.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+        // Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in);
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
+        String nomeTitular = scanner.nextLine();
+        double saldo = scanner.nextDouble();
+
+        ContaBancaria conta = new ContaBancaria(numeroConta, nomeTitular, saldo);
+
+        System.out.println("Informacoes:");
+        System.out.println("Conta: " + conta.numero);
+        System.out.println("Titular: " + conta.titular);
+        System.out.println("Saldo: R$ " + conta.saldo);
+    }
+}
+
+class ContaBancaria {
+    int numero;
+    String titular;
+    double saldo;
+
+    public ContaBancaria(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+}
